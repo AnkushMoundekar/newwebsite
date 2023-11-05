60b54793b8908b1dc217fbb28b7fe280440ce945
Ankush Moundekar <moundekarankush@gmail.com>
2023-11-05 14:19:13 +0530

    my commit

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Products(models.Model):
+    name:models.CharField(max_length=30)
+    descciption:models.CharField(max_length=200)
+    image:models.ImageField
